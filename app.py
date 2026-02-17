4fd5143b88d88c301196a760cf5bf1c4a1e5efba
zenorvin-a11y <zenorvin@gmail.com>
2026-02-17 23:54:30 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
 import os
 
+# Создаём приложение
 app = Flask(__name__)
 
+# Это главная страница
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Это страница "О нас"
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Это страница "Контакты"
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Запуск сервера (это не трогай)
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
